bda9fa642de6c87094203f805dda1e0aab4e67d8
WMinerva <nicolas.ochoa1697@gmail.com>
2025-09-02 15:17:52 -0400

    obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -29,6 +29,19 @@ return {
                 path = "~/Workspace/Notes/",
             },
         },
+        templates = {
+            folder = "Templates",
+            date_format = "%Y-%m-%d",
+            time_format = "%H:%M",
+            -- A map for custom variables, the key should be the variable and the value a function.
+            -- Functions are called with obsidian.TemplateContext objects as their sole parameter.
+            -- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#substitutions
+            substitutions = {},
+
+            -- A map for configuring unique directories and paths for specific templates
+            --- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#customizations
+            customizations = {},
+        },
 
         -- see below for full list of options 👇
     },
